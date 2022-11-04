e44a54bb0e8d024d7200bfdf7fcd873a1d3026df
AlessandroRuzzi <alrxruzzi98@gmail.com>
2022-11-04 21:46:33 +0100

    u

--- data_loader.py
+++ data_loader.py
@@ -62,7 +62,7 @@ def get_train_test_loader(data_dir,
     # load dataset
     folder_path = 'data/subjects'
     file_path = os.path.join(folder_path, all_subjects[subject_id][:-3] +'_calibration.txt')
-    train_set = GazeDataset(dataset_path="/data/aruzzi/person_specific", keys_to_use=datastore["test_specific"],
+    train_set = GazeDataset(dataset_path="/local/home/aruzzi/personal_calibration_files", keys_to_use=datastore["test_specific"],
                             transform=trans, is_shuffle=is_shuffle, index_file=file_path, subject_id=subject_id, is_train=True)
     train_loader = DataLoader(train_set, batch_size=batch_size, num_workers=num_workers)
 
