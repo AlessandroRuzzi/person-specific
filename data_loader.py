9e9c4ffb05d777f4e678f75030eda1ce29ff4496
AlessandroRuzzi <alrxruzzi98@gmail.com>
2022-11-05 16:32:33 +0100

    u

--- data_loader.py
+++ data_loader.py
@@ -110,9 +110,9 @@ class GazeDataset(Dataset):
             if is_train:
                 self.idx_to_kv = []
                 content = np.loadtxt(index_file, dtype=np.float)
-                #self.idx_to_kv = content[:, 0].astype(np.int)
+                self.idx_to_kv = content[:, 0].astype(np.int)
                 #self.idx_to_kv += [i for i in range(len(content))]
-                self.idx_to_kv = content[0:2, 0].astype(np.int)
+                #self.idx_to_kv = content[0:2, 0].astype(np.int)
                 self.gaze_labels_train = content[:, 1:3]
             else:
                 self.idx_to_kv = np.loadtxt(index_file, dtype=np.int)
