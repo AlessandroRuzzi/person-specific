31afac73798a4219c2216bbc1116e1a272d81658
AlessandroRuzzi <alrxruzzi98@gmail.com>
2022-11-06 15:51:55 +0100

    u

--- config.py
+++ config.py
@@ -40,7 +40,7 @@ train_arg.add_argument('--init_lr', type=float, default=0.00001,
                        help='Initial learning rate value')
 train_arg.add_argument('--lr_patience', type=int, default=10,
                        help='Number of epochs to wait before reducing lr')
-train_arg.add_argument('--train_patience', type=int, default=50,
+train_arg.add_argument('--train_patience', type=int, default=5,
                        help='Number of epochs to wait before stopping train')
 train_arg.add_argument('--lr_decay_factor', type=int, default=0.1,
                        help='Number of epochs to wait before reducing lr')
