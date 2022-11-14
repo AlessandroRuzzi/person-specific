652d5b7cb42983dde19a8e6a645628dbfbf4fb14
AlessandroRuzzi <alrxruzzi98@gmail.com>
2022-11-14 17:43:14 +0100

    u

--- trainer.py
+++ trainer.py
@@ -17,7 +17,7 @@ import json
 
 from tqdm import tqdm
 from utils import AverageMeter
-from new_model import gaze_net
+from model import gaze_net
 from linear_model import gaze_net as linear_gaze_net 
 from vgg_model import gaze_network
 from meta_learning import MAML
@@ -256,9 +256,9 @@ class Trainer(object):
         # load the most recent checkpoint
         if self.resume:
             self.load_checkpoint(best=True, is_strict=False,
-                                 input_file_name='ckpt/epoch_24_resnet_correct_ckpt.pth.tar')
+                                 #input_file_name='ckpt/epoch_24_resnet_correct_ckpt.pth.tar')
                                  #input_file_name='ckpt/epoch_24_VGG_80_subj_ckpt.pth.tar')
-                                 #input_file_name='ckpt/ram_epoch_24_ckpt.pth.tar')
+                                 input_file_name='ckpt/ram_epoch_24_ckpt.pth.tar')
             # self.model.locator.gaze_network.load_state_dict(self.model.sensor.gaze_network.state_dict())
             #for param in self.model.parameters():
             #    param.requires_grad = False
