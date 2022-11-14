433c06ffd74fdb3d79c5285fa7b1899c2383af7b
AlessandroRuzzi <alrxruzzi98@gmail.com>
2022-11-14 18:28:30 +0100

    u

--- trainer.py
+++ trainer.py
@@ -367,9 +367,9 @@ class Trainer(object):
             self.batch_size = input_var.shape[0]
 
             pred_gaze, pred_head = self.model(input_var)
-            #pred_gaze = self.reg_gt.predict(pred_gaze.cpu().data.numpy())
+            pred_gaze = self.reg_gt.predict(pred_gaze.cpu().data.numpy())
             #pred_gaze = self.reg_gt.predict(self.poly.transform(pred_gaze.cpu().data.numpy()))
-            pred_gaze = self.linear_model(pred_gaze)
+            #pred_gaze = self.linear_model(pred_gaze)
             #pred_gaze, pred_head = self.meta_model.model(input_var)
             pred_gaze_np = pred_gaze#.cpu().data.numpy()
             prediction_all.append(pred_gaze_np)
