187b47c5f91e96485f1c0f867f3ec55f8aabf873
AlessandroRuzzi <alrxruzzi98@gmail.com>
2022-11-18 13:53:15 +0100

    u

--- data_loader.py
+++ data_loader.py
@@ -101,7 +101,7 @@ class GazeDataset(Dataset):
 
         for num_i in range(0, len(self.selected_keys)):
             if is_train:
-                file_path = os.path.join(self.path, self.selected_keys[num_i][:-3] + "_nsample_7_iter_0.h5")
+                file_path = os.path.join(self.path, self.selected_keys[num_i][:-3] + "_nsample_8_iter_0.h5")
                 #file_path = os.path.join(self.path, self.selected_keys[num_i])
             else:
                 file_path = os.path.join(self.path, self.selected_keys[num_i])
@@ -182,7 +182,7 @@ class GazeDataset(Dataset):
 
         # if self.hdf is None:
         if self.is_train:
-            self.hdf = h5py.File(os.path.join(self.path, self.selected_keys[0][:-3] + "_nsample_7_iter_0.h5"), 'r', swmr=True)
+            self.hdf = h5py.File(os.path.join(self.path, self.selected_keys[0][:-3] + "_nsample_8_iter_0.h5"), 'r', swmr=True)
             #self.hdf = h5py.File(os.path.join(self.path, self.selected_keys[0]), 'r', swmr=True)
         else:
             self.hdf = h5py.File(os.path.join(self.path, self.selected_keys[0]), 'r', swmr=True)
